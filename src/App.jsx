a4cfdcd3cfdf77e4778c5195a2bf0d341cb9f9f4
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2023-02-02 23:18:40 +0400

    Move AllProviders from main to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import ErrorBoundary from "~/components/ErrorBoundary";
 import Layout from "~/components/Layout";
 import ApplicationsList from "~/features/applications/components/List";
-
+import AllProviders from "~/providers/AllProviders";
 
 
 function App() {
   return (
-    <ErrorBoundary>
-      <Layout>
-        <ApplicationsList />
-      </Layout>
-    </ErrorBoundary>
+    <AllProviders>
+      <ErrorBoundary>
+        <Layout>
+          <ApplicationsList />
+        </Layout>
+      </ErrorBoundary>
+    </AllProviders>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AllProviders from "~/providers/AllProviders";
 import App from "./App";
 import "./index.css";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AllProviders>
-      <App />
-    </AllProviders>
+    <App />
   </React.StrictMode>
 );
