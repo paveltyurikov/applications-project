913addaec2cb28b13466fec7db3a9005e7ff5fac
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2023-02-02 18:41:16 +0400

    Remove mocks

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import initApp from "~/lib/initApp.js";
 import AllProviders from "~/providers/AllProviders";
 import App from "./App";
 import "./index.css";
-
-
-
-initApp();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,21 +1,5 @@
 import "@testing-library/jest-dom";
 import { configure } from "@testing-library/react";
-import { server } from "./mocks/server";
 
 
 configure({ testIdAttribute: "data-testid" });
-
-beforeAll(() => {
-  // Enable the mocking in tests.
-  server.listen({ onUnhandledRequest: "error" });
-});
-
-afterEach(() => {
-  // Reset any runtime handlers tests may use.
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  // Clean up once the tests are done.
-  server.close();
-});
